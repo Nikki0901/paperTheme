6fc3ffb344e28d5188557e8b2772225e82766c65
Nitin <saininks2013@gmail.com>
2020-08-18 17:03:30 +0530

    form update

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,24 +1,97 @@
 import React from 'react';
-import LoginForm from './LoginForm'
-import { Container, Row, Col, Jumbotron, Card, CardBody } from "reactstrap";
+import {
+   Col, Form,
+  FormGroup, Label, 
+  Button,
+} from 'reactstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useForm } from "react-hook-form";
+import './login-form.css'
 
 
-export default function App() {
+
+const Login = () => {
+
+  const { handleSubmit, register, errors,reset } = useForm();
+  // const URL = 'http://api.com';
+  
+  const onSubmit = data => {
+    console.log("data:",data);
+    // const resp = fetch(URL).then(res => {
+    //   console.log(res)
+    // });
+  }
+
     return (
-      <div className="App">
-        <Container>
-          <Row>
-            <Col />
-            <Col lg="8">                            
-                <Card>               
-                    <LoginForm />             
-                </Card>             
-            </Col>
-            <Col />
-          </Row>
-        </Container>
+      <div>
+  
+        
+      
+        <Form  className="form" onSubmit={handleSubmit(onSubmit)}>
+        <FormGroup>
+            <Col>
+             <h2 className="form_title">sign in</h2>
+             </Col>
+             </FormGroup>
+          <Col>
+            <FormGroup>
+              <Label>Email</Label>
+              <input
+                name="email"
+                placeholder="Enter Email.."
+                className="input_Field"
+                ref={register({
+                  required: "Required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "invalid email address"
+                  }
+                })}
+              />
+               <p className="error">{errors.email && errors.email.message}</p> 
+            </FormGroup>
+          </Col>
+
+          <Col>
+            <FormGroup>
+              <Label>Password</Label>
+              <input 
+                name="password"
+                type="password"
+                className="input_Field"
+                placeholder="Enter password.."
+                ref={register({
+                  required: "Required",    
+                })}
+                />
+               {errors.password && <p className="error">This field is required</p>}
+            </FormGroup>
+          </Col>
+
+          <Col>
+            <FormGroup>
+            <Button className="form-btn" type="submit">Login</Button>
+              <Button
+                className="form-btn"
+                type="Button"
+                onClick={() => {
+                  reset({
+                    reactSelect: '',
+                  });
+                }}
+                >
+                Reset 
+              </Button>
+            </FormGroup>
+          </Col>
+
+        </Form>
+
       </div>
     );
   }
+
+
+  export default Login;
+
 
